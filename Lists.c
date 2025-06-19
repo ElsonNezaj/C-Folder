f5b220aa8830e1e792b23bff7979e4c0eeeed030
elson <elsonnezaj2005@gmail.com>
2025-06-19 20:18:29 +0200

    new file for lists , need to study it more

--- Lists.c
+++ Lists.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
-typedef struct Node
+typedef struct node
 {
   int data;
   struct node *next;
@@ -44,4 +44,32 @@ node *createList(int n)
 
   return head;
   // KTHEN LISTEN E RE TE KRIJUAR
+}
+
+void addToNode(node **head, int n, int position)
+{
+  // Krijimi i nje node te ri qe duam te shotjme tek lista jone
+  node *newNode = (node *)malloc(sizeof(node));
+  newNode->data = n;
+  newNode->next = NULL;
+
+  // Nese pozicioni eshte 0, e shtojme ne fillim te listes
+  if (position == 0)
+  {
+    newNode->next = *head; // Caktojme te gjithe listen pas node te pare
+    *head = newNode;       // Caktojme node e ri si node te pare
+    return;
+  }
+
+  node *current = *head; // Ruajme listen tone aktuale
+
+  // Krijojme nje loop qe nuk ndryshon listen deri ne momentin qe arrijme ne pozicionin tone
+  for (int i = 0; current != NULL && i < position < 1; i++)
+  {
+    current = current->next;
+  }
+
+  // Pasi arrijme ne pozicionin tone, cfare ndodhet pas pozicionit tone do te jete adresa per elementin e ri
+  newNode->next = current->next;
+  current->next = newNode; // Ne piken e caktuar, vendosim elemetin tone te ri
 }
