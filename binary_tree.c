bf0696fa599dabc15c032d9892e086935e7dfb59
elson <elsonnezaj2005@gmail.com>
2025-06-25 20:28:17 +0200

    working on trees

--- binary_tree.c
+++ binary_tree.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-typedef struct
+typedef struct Node
 {
 	int data;
-	Node *left;	 // Ruan pointer (adresen) e node ne te djathte
-	Node *right; // Ruan pointer (adresen) e node ne te majte
+	struct Node *left;
+	struct Node *right;
 } Node;
 
 Node *GetNewNode(int data)
@@ -33,11 +33,76 @@ Node *Insert(Node *rootPointer, int data)
 	return rootPointer;
 }
 
+void Search(Node *rootPointer, int data) // Changed return type from void* to void
+{
+	if (rootPointer == NULL) // Need to check for NULL pointer
+	{
+		printf("Data not found!\n");
+		return;
+	}
+
+	if (data == rootPointer->data)
+	{
+		printf("Data found!!!!\n");
+	}
+	else if (data <= rootPointer->data)
+	{
+		Search(rootPointer->left, data);
+	}
+	else
+	{
+		Search(rootPointer->right, data);
+	}
+}
+
+void FindNegative(Node *rootPointer)
+{
+	if (rootPointer != NULL)
+	{
+	}
+
+	FindNegative(rootPointer->left);
+	printf("%d \n", rootPointer->data);
+	FindNegative(rootPointer->right);
+}
+
+void PrintTree(Node *root)
+{
+	if (root == NULL)
+	{
+		return;
+	}
+
+	printf("%d\n", root->data);
+
+	if (root->left != NULL)
+	{
+		printf("Left \n");
+		PrintTree(root->left);
+	}
+	if (root->right != NULL)
+	{
+		printf("Right \n");
+		PrintTree(root->right);
+	}
+}
+
 int main()
 {
-	struct Node *rootPointer; // Gjithmone duhet te ruajme adresen (pointer) e rrenjes se pemes
-	rootPointer = NULL;				// Vendosim pemen si bosh
-	Insert(&rootPointer, 15);
-	Insert(&rootPointer, 10);
-	Insert(&rootPointer, 20);
+	Node *rootPointer;
+	rootPointer = NULL;
+	rootPointer = Insert(rootPointer, -7);
+	rootPointer = Insert(rootPointer, 15);
+	rootPointer = Insert(rootPointer, 0);
+	rootPointer = Insert(rootPointer, -3);
+	rootPointer = Insert(rootPointer, 42);
+	rootPointer = Insert(rootPointer, 9);
+
+	// Search(rootPointer, 20);
+	// Search(rootPointer, 99);
+	// InOrder(rootPointer);
+	// FindNegative(rootPointer);
+	PrintTree(rootPointer);
+
+	return 0;
 }
