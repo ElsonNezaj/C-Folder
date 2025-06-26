0c3ba6e555b4fad96b0654b4f3afdecaaf46f58d
elson <elsonnezaj2005@gmail.com>
2025-06-26 11:20:40 +0200

    worked on tests

--- test.c
+++ test.c
@@ -1,72 +1,93 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+///////////////// USHTRIMI 1 ////////////////////
+// int gjejShumen(int vektor[], int n)
+// {
+//   if (n == 0)
+//   {
+//     return 0;
+//   }
 
-typedef struct node
+//   return vektor[0] + gjejShumen(vektor + 1, n - 1);
+// }
+
+///////////////// USHTRIMI 2 ////////////////////
+#define MAX_STUDENTS 200;
+#define MAX_NOTA 10;
+
+typedef struct Student
 {
-  int data;
-  struct node *next;
-} node;
-
-node *createList(int n);
-void displayList(node *head);
+  char name[50];
+  int nrNotash;
+  int *notat;
+} Student;
 
 int main()
 {
-  int n;
-  node *HEAD = NULL;
-  printf("Sa elemente doni ne liste? \n");
-  scanf("%d", &n);
-  HEAD = createList(n);
-  displayList(HEAD);
-  return 0;
-}
+  ///////////////// USHTRIMI 1 ////////////////////
 
-node *createList(int n)
-{
-  int i;
-  node *head = NULL;
-  node *temp = NULL;
-  node *p = NULL;
+  // int vektor[] = {1, 2, 3, 4, 5};
+  // int n = sizeof(vektor) / sizeof(vektor[0]); // gjen size e vektor
+  // printf("%d", gjejShumen(vektor, n));
 
-  for (i = 0; i < n; i++)
+  ///////////////// USHTRIMI 1 ////////////////////
+
+  int i = 0;
+  int maxStudents = MAX_STUDENTS;
+  FILE *file = fopen("students.txt", "r");
+  if (file == NULL)
   {
-    temp = (node *)malloc(sizeof(node));
-    printf("Vendosni vleren per elementin e %d : ", i + 1);
-    scanf("%d", &(temp->data));
-    temp->next = NULL;
+    printf("Nuk mund te hapej file! \n");
+    return 1;
+  }
+  Student *students;
+  students = malloc(maxStudents * sizeof(Student));
 
-    if (head == NULL)
+  int currentStudentSize, numberOfGradesPerStudent;
+  if (fscanf(file, "%d %d", &currentStudentSize, &numberOfGradesPerStudent) != 2)
+  {
+    printf("Gabim në leximin e dimensioneve.\n");
+    fclose(file);
+    return 1;
+  }
+
+  // Ri-alokojme array e studenteve nese kemi me shum student se maksimalja e definuar
+  if (currentStudentSize > maxStudents)
+  {
+    students = realloc(students, currentStudentSize * sizeof(Student));
+  }
+
+  for (int i = 0; i < currentStudentSize; i++)
+  {
+    char emri[50];
+    fscanf(file, "%49[^,]", emri);
+    strcpy(students[i].name, emri);
+
+    // Duke qene se cdo student ka madhesi notash dinamike, na duhet te alokojme madhesine ne heap
+    students[i].notat = malloc(numberOfGradesPerStudent * sizeof(int));
+    if (students[i].notat == NULL)
     {
-      head = temp;
+      printf("Gabim në alokimin e memories për notat e studentit %d.\n", i + 1);
+      break;
     }
-    else
-    {
-      p = head;
+    students[i].nrNotash = numberOfGradesPerStudent;
 
-      while (p->next != NULL)
+    for (int j = 0; j < numberOfGradesPerStudent; j++)
+    {
+      if (fscanf(file, "%d", &students[i].notat[j]) != 1)
       {
-        p = p->next;
+        printf("Gabim ne leximin e notave !\n");
+        students[i].nrNotash = j;
+        break;
       }
-      p->next = temp;
     }
   }
 
-  return head;
-}
+  printf("%d\n", currentStudentSize);
+  printf("%d\n", numberOfGradesPerStudent);
+  printf("%s\n", students[0].name);
 
-void displayList(node *head)
-{
-  node *p = head;
-  while (p != NULL)
-  {
-    if (p->next != NULL)
-    {
-      printf("%d -> ", p->data);
-    }
-    else
-    {
-      printf("%d", p->data);
-    }
-    p = p->next;
-  }
+  fclose(file);
+  return 0;
 }
